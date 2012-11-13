b2f247cab7a1173020c9f7b7af6015504115fdd9
toby cabot <toby@caboteria.org>
2012-11-13 11:27:44 -0500

    fix X_AUTH header so it's json and not inspect format
    The applications behind this gateway evidently expect the X_AUTH
    header to be in json format, but we were just calling "inspect" and
    sending that.  Changed to use to_json instead.

--- proxy_config.rb
+++ proxy_config.rb
@@ -2,4 +2,4 @@
  reverse_proxy_options :preserve_host => false
  keep_headers ["X-AUTH"]
  # Forward the path /* to 'http://localhost:3002/*'
- reverse_proxy '/', 'http://localhost:3002',{ extra_headers: {"X_AUTH"=> lambda{|env| env['warden'].user.inspect}}}
+ reverse_proxy '/', 'http://localhost:3002',{ extra_headers: {"X_AUTH"=> lambda{|env| env['warden'].user.to_json}}}
